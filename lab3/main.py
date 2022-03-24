6bbe6d724a2ae13ba3e89a7f54d8eec7200a5d6a
slicenbeat <slicenbeat@yandex.ru>
2022-03-24 19:03:38 +0400

    Последние изменения

--- lab3/main.py
+++ lab3/main.py
@@ -6,14 +6,15 @@ from models.classes import ImageClass, OBJ3DModel, Point, Color
 # высота, ширина, масштаб, центр, углы поворота
 height = 1000
 weight = 1000
-a_x = 1
-a_y = 1
+a_x = 10
+a_y = 10
 u_0 = height/2
 v_0 = weight/2
 angles = [0, 0, 0]
 # вектор-столбец t, матрица внутренних параметров
 t = np.array([[0.005], [-0.045], [15]])
 intrinsic = np.array([[a_x, 0, u_0], [0, a_y, v_0], [0, 0, 1]])
+# intrinsic = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 
 
 obj = OBJ3DModel('objects/fox.obj')
